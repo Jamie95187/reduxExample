b62876bdb2858c6c29fbc01645bec78398b8433f
Jamie95187 <jamiewong63@googlemail.com>
2020-04-24 21:56:46 +0100

    dispatching actions

--- redux-basics.js
+++ redux-basics.js
@@ -8,6 +8,22 @@ const initialState = {
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+  if (action.type === 'INC_COUNTER') {
+    // THIS IS MUTABLE DONT DO THIS
+    // state.counter++;
+    return {
+      ...state,
+      counter: state.counter + 1
+    };
+  }
+  if (action.type === 'ADD_COUNTER') {
+    // THIS IS MUTABLE DONT DO THIS
+    // state.counter++;
+    return {
+      ...state,
+      counter: state.counter + action.value
+    };
+  }
   return state;
 };
 
@@ -16,5 +32,8 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
 
 // Subscription
