e2af367f80cc408ccbe284a77870dc11617ad4c4
Jamie95187 <jamiewong63@googlemail.com>
2020-04-27 00:33:18 +0100

    refactor helper function for leaner reducer files

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,18 +5,21 @@ const initialState = {
   results: []
 }
 
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(result => result.id !== action.resultElementId);
+  return updateObject(state, {results: updatedArray});
+}
+
 const reducer = (state = initialState, action) => {
   switch ( action.type ) {
     case actionTypes.STORE_RESULT:
     // Change data by multiplying by two
       return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result * 2})});
-
     case actionTypes.DELETE_RESULT:
       // const id = 2;
       // const newArray = [...state.results];
       // newArray.splice(id, 1);
-      const updatedArray = state.results.filter(result => result.id !== action.resultElementId);
-      return updateObject(state, {results: updatedArray});
+      return deleteResult(state, action);
   }
   return state;
 }
