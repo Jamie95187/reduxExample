4a5163c3b86ce5c2e2a0b7c88feb588fe29feb4d
Jamie95187 <jamiewong63@googlemail.com>
2020-04-26 22:31:05 +0100

    adding middleware

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -4,7 +4,7 @@ const initialState = {
   results: []
 }
 
-const reducer =  (state = initialState, action) => {
+const reducer = (state = initialState, action) => {
   switch ( action.type ) {
     case actionTypes.STORE_RESULT:
       return {
